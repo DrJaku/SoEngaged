5b9c57f568469da88d28175ec7534e760482eda3
DrJaku <dror.jakubovitch@gmail.com>
2025-12-29 12:36:45 +0200

    test: moved main.js file

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './src/router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'vue3-toastify/dist/index.css'
